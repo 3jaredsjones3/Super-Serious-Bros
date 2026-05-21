f9a636c76c53c83e098cf9d26fba8e9d8363d0bf
jsjone19 <52929082+jsjone19@users.noreply.github.com>
2026-05-21 01:10:02 -0700

    added fall fast multiplier for gravity

--- super-serious-bros/scenes/player/player.gd
+++ super-serious-bros/scenes/player/player.gd
@@ -4,17 +4,31 @@ extends CharacterBody2D
 @export var gravity: float = 900.0
 @onready var sprite: AnimatedSprite2D = $Visuals
 
+@export var fast_fall_multiplier: float = 1.8
 
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_axis("move_left", "move_right")
 
 	velocity.x = input_dir * move_speed
 
-	if not is_on_floor():
-		velocity.y += gravity * delta #positive y is down the screen so increasing velocity.y moves us downwards faster
+	apply_gravity(delta)
 	move_and_slide()
 
 	update_animation(input_dir)
+
+
+func apply_gravity(delta: float) -> void:
+	if is_on_floor():
+		velocity.y = 0.0
+		return
+
+	var gravity_multiplier := 1.0
+
+	if Input.is_action_pressed("move_down") and velocity.y > 0.0:
+		gravity_multiplier = fast_fall_multiplier
+
+	velocity.y += gravity * gravity_multiplier * delta
+
 
 
 func update_animation(input_dir: float) -> void:
@@ -26,6 +40,7 @@ func update_animation(input_dir: float) -> void:
 		play_anim("run")
 	else:
 		play_anim("idle")
+
 
 
 func play_anim(anim_name: StringName) -> void:
