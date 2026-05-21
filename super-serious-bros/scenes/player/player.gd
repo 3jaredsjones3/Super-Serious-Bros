52e99bd55b175518db8601a0a84f36a2b2da8a99
jsjone19 <52929082+jsjone19@users.noreply.github.com>
2026-05-20 22:11:35 -0700

    Added input controls
    Added input controls and tested switching animated state

--- super-serious-bros/scenes/player/player.gd
+++ super-serious-bros/scenes/player/player.gd
@@ -2,17 +2,29 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $Visuals
 
-func _physics_process(delta: float) -> void:
-	# movement logic here
-	update_animation()
 
-func update_animation() -> void:
-	if not is_on_floor():
-		if velocity.y < 0.0:
-			sprite.play("jump")
-		else:
-			sprite.play("fall")
-	elif absf(velocity.x) > 5.0:
-		sprite.play("run")
+func _physics_process(_delta: float) -> void:
+	debug_animation_input()
+
+
+func debug_animation_input() -> void:
+	if Input.is_action_pressed("move_right"):
+		play_anim("run")
+		sprite.flip_h = false
+		print_debug("Playing run, facing right")
+
+	elif Input.is_action_pressed("move_left"):
+		play_anim("double_jump")
+		sprite.flip_h = true
+		print_debug("Playing double_jump, facing left")
+
 	else:
-		sprite.play("idle")
+		play_anim("idle")
+		print_debug("Playing idle")
+
+
+func play_anim(anim_name: StringName) -> void:
+	if sprite.animation == anim_name:
+		return
+
+	sprite.play(anim_name)
