03ec727083fb9c1708115b6dc8561a5ee479270a
jsjone19 <52929082+jsjone19@users.noreply.github.com>
2026-05-21 04:25:19 -0700

    Implemented wall slide in player script

--- super-serious-bros/scenes/player/player.gd
+++ super-serious-bros/scenes/player/player.gd
@@ -4,12 +4,17 @@ extends CharacterBody2D
 @export var gravity: float = 900.0
 @onready var sprite: AnimatedSprite2D = $Visuals
 
+@onready var wall_check_left: RayCast2D = $WallCheckLeft
+@onready var wall_check_right: RayCast2D = $WallCheckRight
+
 @export var fast_fall_multiplier: float = 1.8
 @export var jump_velocity: float = -300.0
 @export var double_jump_velocity: float = -280.0 #Jump slightly slower on the second upwards jump
 @export var jump_cut_multiplier: float = 0.45
+@export var wall_slide_speed: float = 55.0
 
 var can_double_jump: bool = false
+var is_wall_sliding: bool = false
 
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_axis("move_left", "move_right")
@@ -24,6 +29,7 @@ func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	handle_jump()
 	handle_jump_cut()
+	handle_wall_slide(input_dir)
 	move_and_slide()
 
 	update_after_move()
@@ -74,7 +80,45 @@ func update_after_move() -> void:
 		can_double_jump = true
 
 
+func handle_wall_slide(input_dir: float) -> void:
+	is_wall_sliding = false #start assuming we're not and make it prove that we are
+	
+	#If we're on the floor or moving upward, we're not wall sliding
+	if is_on_floor() or velocity.y <= 0.0:
+		return
+	
+	var wall_dir: int = get_wall_direction()
+	
+	#If we're not in contact with a wall, then we're not wall sliding
+	if wall_dir == 0:
+		return
+
+	#Even if we are in contact with a wall, if we're not pressing against it then we're not wall sliding
+	if signf(input_dir) != wall_dir:
+		return
+	
+	#By this point, we're wall sliding and we want to mark that so update_animation() in _physics_process() can see it
+	is_wall_sliding = true
+	# Let slow falls stay slow, but cap fast falls to wall_slide_speed
+	# Note that we already ruled out upwards movement up above
+	velocity.y = minf(velocity.y, wall_slide_speed)
+
+
+
+func get_wall_direction() -> int:
+	if wall_check_left.is_colliding():
+		return -1
+	if wall_check_right.is_colliding():
+		return 1
+	
+	return 0
+
+
 func update_animation() -> void:
+	if is_wall_sliding:
+		play_anim("wall_slide")
+		return
+	
 	if sprite.animation == "double_jump" and sprite.is_playing():
 		return
 	
